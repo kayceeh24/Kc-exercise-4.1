59eec4a6fdd9d13ec41aad52b5a013e5da9c8b9b
KC Chester Chester <113367258+kayceeh24@users.noreply.github.com>
2022-12-06 00:53:00 +0000

    added all equal sign

--- main.py
+++ main.py
@@ -5,28 +5,27 @@
 while True:
     grade = input("Enter a percent grade: ")
     grade  = int(grade)
-    print("")
     
     if grade < 0:
         print("Invalid grade! Try again.")
-    elif grade < 49:
-        #print("")
+    elif grade <= 49:
+        print("")
         print(f"{grade}% is R")
         break
-    elif grade < 59:
-        #print("")
+    elif grade <= 59:
+        print("")
         print(f"{grade}% is Level 1")
         break
-    elif grade < 69:
-        #print("")
+    elif grade <= 69:
+        print("")
         print(f"{grade}% is Level 2")
         break
-    elif grade < 79:
-        #print("")
+    elif grade <= 79:
+        print("")
         print(f"{grade}% is Level 3")
         break
     elif grade <= 100:
-        #print("")
+        print("")
         print(f"{grade}% is Level 4")
         break
     else:
