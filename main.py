50e222ea7370052cf5d5625adbb3d46e8eaf022f
KC Chester Chester <113367258+kayceeh24@users.noreply.github.com>
2022-12-06 00:48:22 +0000

    changed the print

--- main.py
+++ main.py
@@ -5,27 +5,28 @@
 while True:
     grade = input("Enter a percent grade: ")
     grade  = int(grade)
+    print("")
     
     if grade < 0:
         print("Invalid grade! Try again.")
     elif grade < 49:
-        print("")
+        #print("")
         print(f"{grade}% is R")
         break
     elif grade < 59:
-        print("")
+        #print("")
         print(f"{grade}% is Level 1")
         break
     elif grade < 69:
-        print("")
+        #print("")
         print(f"{grade}% is Level 2")
         break
     elif grade < 79:
-        print("")
+        #print("")
         print(f"{grade}% is Level 3")
         break
-    elif grade < 100:
-        print("")
+    elif grade <= 100:
+        #print("")
         print(f"{grade}% is Level 4")
         break
     else:
