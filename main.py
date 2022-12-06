de8fc5d77f7eabc5cb9d7efd9e185951ad33651d
KC Chester Chester <113367258+kayceeh24@users.noreply.github.com>
2022-12-06 15:42:10 +0000

    a lot

--- main.py
+++ main.py
@@ -2,29 +2,17 @@
 # Helped By: 
 
 # Function for validates a percent grade depends on the users entered and then prints the level grade
-while True:
-    grade = input("Enter a percent grade: ")
-    grade = int(grade)
-    
-    if grade <= 49 and grade >=0:
-        print("")
-        print(f"{grade}% is R")
-        break
-    elif grade <= 59:
-        print("")
-        print(f"{grade}% is Level 1")
-        break
-    elif grade <= 69:
-        print("")
-        print(f"{grade}% is Level 2")
-        break
-    elif grade <= 79:
-        print("")
-        print(f"{grade}% is Level 3")
-        break
-    elif grade <= 100:
-        print("")
-        print(f"{grade}% is Level 4")
-        break
-    else:
-        print("Invalid grade! Try again.")
+percent_grade = int(input("Enter a percent grade: "))
+while percent_grade < 0 or percent_grade > 100:
+    print("Invalid grade! Try again.")
+    grade_percent = int(input("Enter a percent grade: "))
+if percent_grade < 50:
+    print(f"{percent_grade}% is Level R")
+elif percent_grade > 49 and percent_grade < 60:
+    print(f"{percent_grade}% is Level 1")
+elif percent_grade > 59 and percent_grade < 70:
+    print(f"{percent_grade}% is Level 2")
+elif percent_grade > 69 and percent_grade < 80:
+    print(f"{percent_grade}% is Level 3")
+elif percent_grade > 79 and percent_grade < 101:
+    print(f"{percent_grade}% is Level 4")
