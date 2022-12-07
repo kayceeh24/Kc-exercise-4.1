87c71c6cf700a3a440dc42e4e825fd46f79e10a0
KC Chester Chester <113367258+kayceeh24@users.noreply.github.com>
2022-12-07 01:57:08 +0000

    a lot

--- main.py
+++ main.py
@@ -5,14 +5,19 @@
 percent_grade = int(input("Enter a percent grade: "))
 while percent_grade < 0 or percent_grade > 100:
     print("Invalid grade! Try again.")
-    grade_percent = int(input("Enter a percent grade: "))
+    percent_grade = int(input("Enter a percent grade: "))
 if percent_grade < 50:
+    print("")
     print(f"{percent_grade}% is Level R")
-elif percent_grade > 49 and percent_grade < 60:
+elif percent_grade < 60:
+    print("")
     print(f"{percent_grade}% is Level 1")
-elif percent_grade > 59 and percent_grade < 70:
+elif percent_grade < 70:
+    print("")
     print(f"{percent_grade}% is Level 2")
-elif percent_grade > 69 and percent_grade < 80:
+elif percent_grade < 80:
+    print("")
     print(f"{percent_grade}% is Level 3")
-elif percent_grade > 79 and percent_grade < 101:
+elif percent_grade < 101:
+    print("")
     print(f"{percent_grade}% is Level 4")
